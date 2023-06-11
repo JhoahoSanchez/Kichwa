77ca5739c87d700adea654fb606c73c475ae8f07
Lis181201 <lisbeth.romo36@gmail.com>
2023-06-11 18:01:15 -0500

    Corrección de código

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
         numeroKichwa.agregarNumero(new NumeroKichwa(0, "Illak"),
                 new NumeroKichwa(1, "Shuk"),
                 new NumeroKichwa(2, "Ishkay"),
-                new NumeroKichwa(3, "kimsa"),
+                new NumeroKichwa(3, "Kimsa"),
                 new NumeroKichwa(4, "Chusku"),
                 new NumeroKichwa(5, "Pishka"),
                 new NumeroKichwa(6, "Shukta"),
@@ -25,14 +25,14 @@ public class Main {
         String respuesta = "";
         String resultado = "";
         do {
-            numeroIngresado = JOptionPane.showInputDialog("\tConversor de numeros a kichwa\nIngrese un número:\n");
+            numeroIngresado = JOptionPane.showInputDialog("\tConversor de números a kichwa\nIngrese un número:\n");
             if (validacion.validarNumero(numeroIngresado)) {
                 resultado = conversor.convertirNumero(Integer.parseInt(numeroIngresado));
-                JOptionPane.showMessageDialog(null, ("Numero en kichwa: " + resultado));
+                JOptionPane.showMessageDialog(null, ("Número en kichwa: " + resultado));
             } else {
                 JOptionPane.showMessageDialog(null, "Introduzca un número entre el 0 y 9999");
             }
-            respuesta = JOptionPane.showInputDialog("Desea introducir otro numero? si/no");
+            respuesta = JOptionPane.showInputDialog("¿Desea introducir otro número? si/no");
 
         } while (respuesta.toLowerCase().equals("si") || respuesta.toLowerCase().equals("s"));
     }
